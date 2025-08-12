e1a2878436555df5b077c4ceccc9045b22348efa
Angelo <palebinoangelo42@gmail.com>
2025-08-12 20:12:23 +0800

    added verification to admin login

--- components/Admin_Form/page.tsx
+++ components/Admin_Form/page.tsx
@@ -1,9 +1,19 @@
+'use client';
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Eye, EyeOff } from "lucide-react";
-import { DialogClose } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { VerificationForm } from "../Verification_form/page";
 
 interface LoginProps {
   onSwitchToLogin: () => void;
@@ -11,6 +21,13 @@ interface LoginProps {
 
 export function AdminLogin({ onSwitchToLogin }: LoginProps) {
   const [showPassword, setShowPassword] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const handleVerificationSubmit = (code: string) => {
+    console.log("Verification code entered:", code);
+    // TODO: Call your API or complete registration flow here
+    setOpen(false);
+  };
 
   return (
     <form className="flex flex-col flex-grow" method="POST" action="/api/login">
@@ -64,11 +81,31 @@ export function AdminLogin({ onSwitchToLogin }: LoginProps) {
 
       <div className="mt-auto flex justify-end gap-2">
         <DialogClose asChild>
-          <Button variant="outline" type="button" formNoValidate>
+          <Button variant="outline" type="button" formNoValidate className="hover:cursor-pointer">
             Cancel
           </Button>
         </DialogClose>
-        <Button type="submit">Log In</Button>
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <Button type="button" className="hover:cursor-pointer">
+              Login
+            </Button>
+          </DialogTrigger>
+
+          <DialogContent>
+            <DialogHeader className="flex justify-center items-center">
+              <DialogTitle>Account Verification</DialogTitle>
+              <DialogDescription>
+                Please enter the OTP sent to your email.
+              </DialogDescription>
+            </DialogHeader>
+
+            <VerificationForm
+              onSubmit={handleVerificationSubmit}
+              onCancel={() => setOpen(false)}
+            />
+          </DialogContent>
+        </Dialog>
       </div>
     </form>
   );
